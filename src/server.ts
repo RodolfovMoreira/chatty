9b9b37c7457e5d3cbb257935320f7fd7c02a6671
Rodolfo Moreira <rodolfomoreira1994@gmail.com>
2021-06-30 22:06:48 -0300

    Class 02

--- src/server.ts
+++ src/server.ts
@@ -1,26 +1,13 @@
 import express from "express";
 
+import "./database";
+import { routes } from "./routes";
+
 const app = express();
 
-/**
- * GET    -> Searches
- * POST   -> Creation
- * PUT    -> Modifications
- * DELETE -> Remove
- * PATCH  -> Modifying specific informations (change client password)
- */
+app.use(express.json());
 
-app.get("/", (request, response) => {
-  return response.json({
-    message: "Olá NLW 05!",
-  });
-});
-
-app.post("/", (request, response) => {
-  return response.json({
-    message: "Usuário salvo com sucesso!"
-  });
-});
+app.use(routes);
 
 app.listen(3333, () => console.log("Server is running on port 3333"));
 
